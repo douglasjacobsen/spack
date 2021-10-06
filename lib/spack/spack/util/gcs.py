efeeb8bbcdb68baeef0005dbbf6a1111d7055e11
Douglas Jacobsen <jacobsen.douglas@gmail.com>
2021-10-06 03:03:47 +0000

    FU

--- lib/spack/spack/util/gcs.py
+++ lib/spack/spack/util/gcs.py
@@ -76,16 +76,18 @@ class GCSBucket(object):
             self.bucket = self.client.create_bucket(self.name)
 
     def get_blob(self, blob_path):
+        tty.debug("Bucket.get_blob({0})".format(blob_path))
         if self.exists():
             return self.bucket.get_blob(blob_path)
         return None
 
     def blob(self, blob_path):
+        tty.debug("Bucket.blob({0})".format(blob_path))
         if self.exists():
             return self.bucket.blob(blob_path)
         return None
 
-    def get_all_blobs(self, recursive=True):
+    def get_all_blobs(self, recursive=True, relative=True):
         """Get a list of all blobs
         Returns a list of all blobs within this bucket.
 
@@ -97,21 +99,38 @@ class GCSBucket(object):
         """
         tty.debug('Getting GCS blobs... Recurse {0}'.format(
             recursive))
+
+        if relative:
+            converter = self._relative_blob_name
+        else:
+            converter = str
+
         if self.exists():
             all_blobs = self.bucket.list_blobs(prefix=self.prefix)
             blob_list = []
 
-            base_dirs = len(self.prefix.split('/'))
+            base_dirs = len(self.prefix.split('/')) + 1
 
             for blob in all_blobs:
                 if not recursive:
                     num_dirs = len(blob.name.split('/'))
+                    tty.debug("GAB: {0} -- {1} <= {2}".format(blob.name, num_dirs, base_dirs))
                     if num_dirs <= base_dirs:
-                        blob_list.append(blob.name)
+                        blob_list.append(converter(blob.name))
                 else:
-                    blob_list.append( blob.name )
+                    blob_list.append(converter(blob.name))
 
+            tty.debug("All blobs = {0}".format(blob_list))
             return blob_list
+
+    def _relative_blob_name(self, blob_name):
+        return os.path.relpath(blob_name, self.prefix)
+        #parts = blob_name.split('/')
+        #try:
+        #    pe_index = parts.index(self.prefix_end)
+        #    return os.path.join(*parts[pe_index + 1:])
+        #except ValueError:
+        #    return os.path.join(*parts[:])
 
     def destroy(self, recursive=False, **kwargs):
         """Bucket destruction method
@@ -121,8 +140,9 @@ class GCSBucket(object):
         Uses GCS Batch operations to bundle several delete operations together.
         """
         from google.cloud.exceptions import NotFound
+        tty.debug("Bucket.destroy(recursive={0})".format(recursive))
         try:
-            bucket_blobs = self.get_all_blobs(recursive=recursive)
+            bucket_blobs = self.get_all_blobs(recursive=recursive, relative=False)
             batch_size = 1000
 
             num_blobs = len(bucket_blobs)
@@ -156,12 +176,10 @@ class GCSBlob(object):
 
         self.bucket = GCSBucket(url)
 
-        if self.url.path[0] == '/':
-            self.blob_path = self.url.path[1:]
-        else:
-            self.blob_path = self.url.path
+        self.blob_path = self.url.path.lstrip('/')
 
-        tty.debug("blob_path = {0}".format(self.blob_path))
+        tty.debug("New GCSBlob")
+        tty.debug("  blob_path = {0}".format(self.blob_path))
 
         if not self.bucket.exists():
             tty.warn("The bucket {0} does not exist, it will be created"
@@ -169,27 +187,32 @@ class GCSBlob(object):
             self.bucket.create()
 
     def get(self):
+        tty.debug("Blob.get()")
         return self.bucket.get_blob(self.blob_path)
 
     def exists(self):
+        from google.cloud.exceptions import NotFound
         try:
             blob = self.bucket.blob(self.blob_path)
             exists = blob.exists()
-        except Exception:
+        except NotFound:
             return False
 
         return exists
 
     def delete_blob(self):
+        from google.cloud.exceptions import NotFound
+        tty.debug("Blob.delete_blob()")
         try:
             blob = self.bucket.blob(self.blob_path)
             blob.delete()
-        except Exception as ex:
+        except NotFound as ex:
             tty.error("{0}, Could not delete gcs blob {1}".format(ex, self.blob_path))
 
     def upload_to_blob(self, local_file_path):
+        tty.debug("Blob.upload_to_blob({0})".format(local_file_path))
         try:
-            blob = self.bucket.blob(self.blob_path.lstrip("/"))
+            blob = self.bucket.blob(self.blob_path)
             blob.upload_from_filename(local_file_path)
         except Exception as ex:
             tty.error("{0}, Could not upload {1} to gcs blob storage"
@@ -197,9 +220,11 @@ class GCSBlob(object):
             sys.exit(1)
 
     def get_blob_byte_stream(self):
+        tty.debug("Blob.get_blob_byte_stream")
         return self.bucket.get_blob(self.blob_path).open(mode='rb')
 
     def get_blob_headers(self):
+        tty.debug("Blob.get_blob_headers")
         blob = self.bucket.get_blob(self.blob_path)
 
         headers = {}
@@ -209,3 +234,7 @@ class GCSBlob(object):
         headers['MD5Hash'] = blob.md5_hash
 
         return headers
+
+    def __str__(self):
+        tty.debug("Blob.str = gs://{0}".format(self.blob_path))
+        return "gs://" + self.blob_path
